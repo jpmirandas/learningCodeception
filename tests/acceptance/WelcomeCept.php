7a8d75498dcbd52a73baa2911e767ddeae4da384
João Paulo Miranda <jpmirandas@gmail.com>
2016-07-28 21:57:12 -0300

    Added the gitignore file. The settings were changes, now, the webDriver is used (PhatomJS).

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -1,6 +1,9 @@
 <?php 
-  $I = new AcceptanceTester($scenario);
-  $I->wantTo('ensure that frontpage works');
-  $I->amOnPage('/');
-  $I->see('Google');
+    $I = new AcceptanceTester($scenario);
+    $I->wantTo('ensure that frontpage works');
+    $I->amOnPage('/');
+    $I->see('Google');
+    $I->click('Estou com sorte');
+    $I->makeScreenshot('screen');
+    $I->seeInCurrentUrl('doodles');
 ?>
