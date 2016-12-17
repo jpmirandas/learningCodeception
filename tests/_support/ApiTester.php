4582872c8cf8b64db0d6ec2c4540f4c214705c54
João Paulo Miranda <jpmirandas@gmail.com>
2016-12-17 19:17:07 -0300

    Added a api test example

---
+++ tests/_support/ApiTester.php
@@ -0,0 +1,6 @@
+<?php
+
+class ApiTester extends \Codeception\Actor {
+
+    use _generated\ApiTesterActions;
+}
---
+++ tests/api/ListAlbunsCept.php
@@ -0,0 +1,6 @@
+<?php
+$I = new ApiTester($scenario);
+$I->wantTo('Request the Ney\'s albuns!');
+$I->sendGET('/search', ['query' => 'ney matogrosso', 'type' => 'album']);
+$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
+$I->seeResponseContains('"id" : "6QIgoxBzURhS6wNOTkxRfO"');
